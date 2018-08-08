83f9d9a2b14bb4cec0b3a2accffafe27a9f1161c
Alvaro Velasco <avf@exerp.com>
2018-08-08 16:08:37 +0200

    Comments

--- bitcoin-trading-core/src/main/java/org/sonar/challenge/book/subscription/difford/DiffOrderOnBookSubscribeFeederImpl.java
+++ bitcoin-trading-core/src/main/java/org/sonar/challenge/book/subscription/difford/DiffOrderOnBookSubscribeFeederImpl.java
@@ -24,7 +24,7 @@ import org.sonar.challenge.websocket.BitsoSubscriber.Handler;
 import org.sonar.challenge.websocket.SubscriptionTypes;
 
 /**
- * 
+ * Class subscribing to feeder on the diff-order channel.
  * @author Alvaro Velasco
  *
  */
--- bitcoin-trading-core/src/main/java/org/sonar/challenge/websocket/SubscriptionTypes.java
+++ bitcoin-trading-core/src/main/java/org/sonar/challenge/websocket/SubscriptionTypes.java
@@ -1,6 +1,7 @@
 package org.sonar.challenge.websocket;
 
 public enum SubscriptionTypes {
+	// All the other types are meaningless at this stage of development.
 	DIFF_ORDERS("diff-orders");
 	private final String keyword;
 
